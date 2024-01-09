38bc5e9a849806f37dc1d00d7f6e53cb3fd6ae7e
jangjisu <wltn85@naver.com>
2024-01-09 20:33:02 +0900

    feat : API Response 값에 httpStatus 코드 추가

--- src/main/java/com/js/secondhandauction/core/item/controller/ItemController.java
+++ src/main/java/com/js/secondhandauction/core/item/controller/ItemController.java
@@ -1,11 +1,13 @@
 package com.js.secondhandauction.core.item.controller;
 
+import com.js.secondhandauction.common.response.ApiResponse;
 import com.js.secondhandauction.core.item.domain.Item;
 import com.js.secondhandauction.core.item.dto.ItemRequest;
 import com.js.secondhandauction.core.item.dto.ItemResponse;
 import com.js.secondhandauction.core.item.service.ItemService;
 import com.js.secondhandauction.core.user.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -16,31 +18,30 @@ public class ItemController {
     @Autowired
     private ItemService itemService;
 
-
     /**
      * 상품 등록
      */
     @PostMapping
-    public ResponseEntity<ItemResponse> createItem(@AuthenticationPrincipal User user, @RequestBody ItemRequest itemRequest) {
+    public ResponseEntity<ApiResponse<ItemResponse>> createItem(@AuthenticationPrincipal User user, @RequestBody ItemRequest itemRequest) {
         final String userId = user.getUsername();
-        return ResponseEntity.ok(itemService.createItem(userId, itemRequest));
+        return new ResponseEntity<>(ApiResponse.success(itemService.createItem(userId, itemRequest)), HttpStatus.CREATED);
     }
 
     /**
      * 상품 조회
      */
     @GetMapping("/{itemNo}")
-    public ResponseEntity<Item> getItem(@PathVariable long itemNo) {
-        return ResponseEntity.ok(itemService.getItem(itemNo));
+    public ResponseEntity<ApiResponse<Item>> getItem(@PathVariable long itemNo) {
+        return new ResponseEntity<>(ApiResponse.success(itemService.getItem(itemNo)), HttpStatus.OK);
     }
 
     /**
      * 상품 수정
      */
     @PutMapping("/{itemNo}")
-    public ResponseEntity<ItemResponse> updateItem(@AuthenticationPrincipal User user, @PathVariable long itemNo, @RequestBody ItemRequest itemRequest) {
+    public ResponseEntity<ApiResponse<ItemResponse>> updateItem(@AuthenticationPrincipal User user, @PathVariable long itemNo, @RequestBody ItemRequest itemRequest) {
         final String userId = user.getUsername();
-        return ResponseEntity.ok(itemService.updateItem(itemNo, userId, itemRequest));
+        return new ResponseEntity<>(ApiResponse.success(itemService.updateItem(itemNo, userId, itemRequest)), HttpStatus.OK);
     }
 
 }
