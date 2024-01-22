12615cb40ad1290f1f21a0c890b0ab05e1fe4296
jangjisu <wltn85@naver.com>
2024-01-22 13:44:19 +0900

    fix : mysql 버전 지정하지 않도록 수정, 사용하지 않는 import 제거

--- src/main/java/com/js/secondhandauction/common/config/SpringSecurityConfig.java
+++ src/main/java/com/js/secondhandauction/common/config/SpringSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.js.secondhandauction.common.config;
 
-import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
