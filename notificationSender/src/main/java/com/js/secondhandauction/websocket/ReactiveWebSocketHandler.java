54904062c66568179f1f3eb833947cfb00363540
jangjisu <wltn85@naver.com>
2024-03-04 15:40:07 +0900

    feat: notificationBroker를 통해 받은 참가자 정보로 websocket을 통해 실시간 알림 전송

---
+++ notificationSender/src/main/java/com/js/secondhandauction/websocket/ReactiveWebSocketHandler.java
@@ -0,0 +1,37 @@
+package com.js.secondhandauction.websocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@Slf4j
+public class ReactiveWebSocketHandler implements WebSocketHandler {
+
+    private final Map<String, Sinks.Many<String>> sinkMap = new ConcurrentHashMap<>();
+
+
+    @Override
+    public Mono<Void> handle(WebSocketSession session) {
+        log.info("Session Opened");
+
+        return session.receive()
+                .map(WebSocketMessage::getPayloadAsText)
+                .doOnNext(userno -> {
+                    Sinks.Many<String> sink = sinkMap.computeIfAbsent(userno, k -> Sinks.many().multicast().directBestEffort());
+                    session.send(sink.asFlux().map(session::textMessage)).subscribe();
+                })
+                .then();
+    }
+
+    public Sinks.Many<String> getSink(String userno) {
+        return sinkMap.get(userno);
+    }
+}
